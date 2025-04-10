c0e0e6ffe72004555c08c11e3fc6a24c42ac3caa
Otagiri-Y <otagiri.yo.hey@gmail.com>
2025-04-10 22:40:40 +0900

    startup.sh関係を追加

---
+++ gunicorn.py
@@ -0,0 +1,26 @@
+"""
+import multiprocessing
+import os
+
+name = "Gunicorn config for FastAPI"
+
+bind = "0.0.0.0:8000"
+
+worker_class = "uvicorn.workers.UvicornWorker"
+workers = multiprocessing.cpu_count () * 2 + 1
+"""
+import multiprocessing
+import os
+
+name = "Gunicorn config for FastAPI"
+
+# 環境変数 PORT を取得
+port = os.getenv('PORT', '8000')
+
+# 確認用に出力
+print(f"Using PORT: {port}")
+
+# バインド設定
+bind = f"0.0.0.0:{port}"
+worker_class = "uvicorn.workers.UvicornWorker"
+workers = multiprocessing.cpu_count() * 2 + 1
