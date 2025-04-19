d8637739fc224fd1329e8e65d85a8683f78b6d50
Otagiri-Y <otagiri.yo.hey@gmail.com>
2025-04-20 01:14:30 +0900

    修正

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from app.routers.company import router as company_router
 from app.routers.job_type import router as job_type_router
 from app.routers.job_type_detail import router as job_type_detail_router
 from app.routers.matching import router as matching_router
-# from app.routers.search import router as search_router
+from app.routers.search import router as search_router
 from app.routers.check_auth import router as check_auth_router
 from fastapi.middleware.cors import CORSMiddleware
-# from app.routers.upload import router as upload_router
+from app.routers.upload import router as upload_router
 import os
 
 app = FastAPI()
@@ -31,9 +31,9 @@ app.include_router(company_router)
 app.include_router(job_type_router)
 app.include_router(job_type_detail_router)
 app.include_router(matching_router)
-# app.include_router(search_router)
+app.include_router(search_router)
 app.include_router(check_auth_router)
-# app.include_router(upload_router)
+app.include_router(upload_router)
 
 @app.get("/")
 def read_root():
